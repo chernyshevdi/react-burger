4d09a2e50f85652cbed9680a70e7dcb3ca30724a
Dmitrii Chernyshev <chernyshevddi@yandex.ru>
2021-12-07 05:19:33 +0300

    finnaly change name

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/changeappname/app.js';
+import App from './components/app/app.js';
 import reportWebVitals from './reportWebVitals';
 import './fonts/fonts.css';
 import { createStore, applyMiddleware, compose } from 'redux';
